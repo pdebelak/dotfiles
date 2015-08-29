b9e919fcd037f603beffd035dded275f1922c123
pdebelak <pdebelak@gmail.com>
2015-08-29 10:46:36 -0500

    Initial setup

---
+++ .zsh.after/alias.zsh
@@ -0,0 +1,26 @@
+# Shell Aliases
+
+# minitest aliases
+alias minitest-'ruby -Ilib:test'
+alias mt='minitest'
+
+# tmux aliases
+alias tm='tmux'
+alias tmn='tm new -s'
+alias tma='tm attach -t'
+alias tms='tm switch -t'
+alias tmls='tm list-sessions'
+
+# DevOps
+name_to_ip () {
+  ip=`knife search node name:$1 | awk '/IP/{print $2;}'`
+}
+ressh () {
+  name_to_ip $1
+  echo "Ssh'ing to $ip"
+  /usr/bin/ssh reverb@$ip
+}
+rescp () {
+  name_to_ip $1
+  noglob scp reverb@$ip:$2 $3
+}}}}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Env configuration
+
+This is my custom shell configuration. It is meant to be used along with [yadr](https://github.com/skwp/dotfiles).
+
+## Instructions
+
+1. clone this repo
+2. `./symlink.sh`
