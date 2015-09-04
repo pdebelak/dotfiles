b36ae1daca8f5db36eea4952cb0dd37bcf9aa2d1
pdebelak <pdebelak@gmail.com>
2015-09-04 10:24:01 -0500

    Add tmux-related spec shortcuts

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This is my custom shell configuration. It is meant to be used along with [yadr](
 
 1. clone this repo
 2. `./symlink.sh`
+3. `./install_plugins.sh`
---
+++ install_plugins.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Adding additional vim plugins to yadr..."
+yadr vim-add-plugin -u https://github.com/pdebelak/spec_commands.vim
+yadr vim-add-plugin -u https://github.com/jgdavey/tslime.vim
