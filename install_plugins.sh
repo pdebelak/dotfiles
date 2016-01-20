517767653cd4bf1817c360dc75daf081001c069c
pdebelak <pdebelak@gmail.com>
2016-01-20 15:32:40 -0600

    Add eslinting for jsx

--- install_plugins.sh
+++ install_plugins.sh
@@ -3,3 +3,5 @@
 yadr vim-add-plugin -u https://github.com/pdebelak/spec_commands.vim
 yadr vim-add-plugin -u https://github.com/jgdavey/tslime.vim
 yadr vim-add-plugin -u https://github.com/t9md/vim-ruby-xmpfilter
+yadr vim-add-plugin -u https://github.com/pangloss/vim-javascript
+yadr vim-add-plugin -u https://github.com/mxw/vim-jsx
