89a4dbdb35bcea194fc973a83e4bd32db3db0b27
pdebelak <pdebelak@gmail.com>
2018-05-30 13:21:55 -0500

    Don't record duplicates in history

--- bash/exports.sh
+++ bash/exports.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:$HOME/bin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export FZF_DEFAULT_COMMAND='rg --files --ignore-file=~/.agignore --follow --glob ""'
+export HISTCONTROL=ignoreboth:erasedups
 export EDITOR='vim'
 if type nvim > /dev/null 2>&1; then
   export EDITOR='nvim'
