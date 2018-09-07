8db8a1a852f1366a615b2e60db80bbf9fc31f485
pdebelak <pdebelak@gmail.com>
2018-09-07 15:56:22 -0500

    Delete trailing whitespace on save

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -48,6 +48,10 @@
     (if filename
 	(kill-new filename))))
 
+;; delete trailing whitespace on save
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)
+
 ;; the package manager
 (require 'package)
 (setq
